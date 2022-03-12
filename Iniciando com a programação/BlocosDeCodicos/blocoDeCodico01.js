ac8425745864de9ae0bd0b06719d3d5123f9829b
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-12 08:41:39 -0300

    criando novos pastas e arquivos

---
+++ Iniciando com a programação/BlocosDeCodicos/blocoDeCodico01.js
@@ -0,0 +1,26 @@
+
+//Aqui abaixo são seguencia de sentenca
+let numero1 = 4
+let numero2 = 6
+let teste = 11
+let soma = numero1 + numero2
+
+//Aqui a baixo e o blocos de código
+if (soma > teste){
+    console.log('A soma é Maior que o teste ')
+    console.log('A soma vale '+soma)
+    console.log('O valor do teste é ' +teste)
+
+};
+
+if (soma < teste){
+    console.log('A soma é Menor que o  teste')
+    console.log('A soma vale '+soma)
+    console.log('O valor do teste é ' +teste)
+}
+else{
+    console.log('A soma e igual')
+}
+
+
+
---
+++ Iniciando com a programação/Tipos de dados/Dados01.js
@@ -0,0 +1,15 @@
+let objeto = 'caneta';
+let numero = 2 ;
+let valor = 342;
+let valorFloat = 4.89;
+let pessoa = 'mariana';
+let soma = 3 + 6;
+let time = 'santos'
+
+console.log(objeto)
+console.log(numero)
+console.log(valor)
+console.log(valorFloat)
+console.log(pessoa)
+console.log(soma)
+console.log(time)
