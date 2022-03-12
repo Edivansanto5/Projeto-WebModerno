8dc896c6f14c8805d4b107932415823de68673a7
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-12 16:54:59 -0300

    criando o arquivo de estrutura de dados da linguagem js

---
+++ Iniciando com a programação/Organização Básica de um Código JS/estruturaDeUmCodigoJS.js
@@ -0,0 +1,27 @@
+//Um codigo em java scrip e basicamente divido em..
+/*
+ Sentenças de codigo terminado ou não com ;
+ Blocos de codigos que abrem e fecha com chaves { } terminado ou não com ; 
+
+*/
+
+console.log('Aqui e uma senteça');
+
+console.log(
+    "Aqui e outra sentença"
+);
+
+{
+    // Aqui e  um bloco de código
+}
+
+{
+    {
+        let numero = 2;
+        let numero2 = 4;
+        let soma = numero * numero2
+        console.log(soma)
+        // aqui e outro bloco de códico
+    }
+    console.log("aqui faz parte desse bloco")
+};
