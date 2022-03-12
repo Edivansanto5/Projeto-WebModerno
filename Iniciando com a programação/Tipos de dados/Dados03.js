b221d2c663f89e675aa583ccb2854ef0abdea235
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-12 13:29:02 -0300

    fazendo um novo arquivo na pasta de tipos de dados

---
+++ Iniciando com a programação/Tipos de dados/Dados03.js
@@ -0,0 +1,19 @@
+/*Vamos ver nessa Aula sobre as importancia de variaveis*/
+
+var a = 3;
+a = 5;
+a = a + 4;
+console.log(a)
+
+let b = 4
+b = 5
+console.log(b)
+
+/*
+const c = 6
+c = 5
+console.log(c)
+
+se vc fizer isso vai da erro porque repare que vc tentou mudar um valor de uma variavel constante e
+isso não e aceitavel na linguagem
+*/
