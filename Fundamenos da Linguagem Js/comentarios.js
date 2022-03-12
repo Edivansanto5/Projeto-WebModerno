f4e759b830e1a80a1a085eea752348b299d6ae89
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-12 17:40:30 -0300

    adcionandos mais arquivos e pastas em funadamentos

---
+++ Fundamenos da Linguagem Js/comentarios.js
@@ -0,0 +1,7 @@
+// os comentarios podem ser de uma linha ou ...
+console.log('Friza porque matou o curinrin')
+
+/*
+    Ou podem ser de diversas linhas
+
+*/
---
+++ Fundamenos da Linguagem Js/variaveis.js
@@ -0,0 +1,15 @@
+// aqui vamos ver os tipos de variaveis em js
+
+var numero = 2
+let numero2 = 4
+console.log(numero,numero2)
+
+var numero = 34
+/*let */ numero2 = 89 // daria erro
+console.log(numero,numero2)
+
+// todos daria erro!
+const numero3 = 56
+//numero3 = 45
+//const numero3 = 98 
+console.log(numero3)
