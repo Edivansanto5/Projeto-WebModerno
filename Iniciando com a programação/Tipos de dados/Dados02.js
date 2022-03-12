5b2240bda447e259d81249606d8615ee4650a544
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-12 13:09:53 -0300

    criando aula de tipos de dados em js

---
+++ Iniciando com a programação/Tipos de dados/Dados02.js
@@ -0,0 +1,16 @@
+// metados para saber os tipos de dados que vc está trabalhando
+//console.log(nome);
+console.log('Vamos ver quais são os tipos de dados disponivel em java script')
+let nome = 'Mariana da sivla';
+console.log('tipo é ' + typeof (nome)); // ttypeof e pra saber qual e o tipo de dados
+
+let preco = 33;
+console.log('tipo é ' + typeof (preco));
+
+let preco01 = 3.89;
+console.log('tipo é ' + typeof (preco01));
+
+let verdadeiro = true;
+let falso = false;
+console.log('tipo é ' + typeof (verdadeiro));
+console.log('tipo é ' + typeof (falso));
