d39cb99eb039f15678a0b406c7232ff1c9c764e7
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-14 20:15:21 -0300

    pasta de let finalizada

---
+++ Fundamenos da Linguagem Js/Variaveis Tipo var/let01.js
@@ -0,0 +1,18 @@
+let nome = 1
+
+{
+    {
+        
+        {
+            {
+                {
+                    {
+                        let nome = 2
+                        console.log('dentro do escopo',nome)
+                    }
+                }
+            }
+        }
+    }
+}
+console.log('fora do escopo',nome)
---
+++ Fundamenos da Linguagem Js/Variaveis Tipo var/var02.js
@@ -0,0 +1,18 @@
+var nome = 1
+
+{
+    {
+        
+        {
+            {
+                {
+                    {
+                        var  nome = 2
+                        console.log(nome)
+                    }
+                }
+            }
+        }
+    }
+}
+console.log(nome)
