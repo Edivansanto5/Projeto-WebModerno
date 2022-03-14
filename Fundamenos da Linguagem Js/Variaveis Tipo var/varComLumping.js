b6994c4215d23a4d482fe80cb5d4a9efbf5ef787
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-14 20:46:58 -0300

    pasta de var com laço de repetção

---
+++ Fundamenos da Linguagem Js/Variaveis Tipo var/varComLumping.js
@@ -0,0 +1,25 @@
+for (var i = 0; i<=10;  i++){
+    console.log(i)
+}
+console.log('i vale',i)
+
+/*
+let raio = 10
+let PI = 3.14
+let areaCir = PI * Math.pow(raio,2)
+console.log(areaCir.toFixed(5))
+*/
+// mine desafio faça de 0 a 150
+for (let i=0; i <150; i++ ){
+    if(i%2==1){
+        console.log(i)
+        
+    }
+    
+}
+for (var i = 0; i<=10;  i++){
+    console.log(i)
+}
+console.log('i vale',i)
+
+
