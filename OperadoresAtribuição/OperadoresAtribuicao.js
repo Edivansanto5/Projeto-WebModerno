318f3940caad5e003c3cf9f82c4c55c27b57ee62
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-17 23:03:12 -0300

    finalizando as atribuição aditiva subtrativa e multiplicativa

---
+++ OperadoresAtribuição/OperadoresAtribuicao.js
@@ -0,0 +1,20 @@
+// atribuição aditiva
+let a = 7;
+let b = 2
+b += a // b = b + a  ou seja o (b) vai receber ele mesmo mais o valor de (a) ou seja 2 + 7 = 9 
+console.log(b,a)
+
+// atribuição subtrativa
+let c = 19
+let d = 5
+c -= d; // c = c - d
+console.log(c)
+
+//tribuição multiplicativa
+let e = 5
+let f = 5
+e *= f; // e = e * f
+console.log(e)
+
+
+
