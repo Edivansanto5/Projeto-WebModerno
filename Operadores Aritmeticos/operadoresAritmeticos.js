3e1e3e936ae609f5ce741626d8837c9dd0ca4ee1
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-21 14:10:07 -0300

    finalizando aula de operadores

--- Operadores Aritmeticos/operadoresAritmeticos.js
+++ Operadores Aritmeticos/operadoresAritmeticos.js
@@ -1,3 +1,9 @@
 const [ a ,b, c, d] = [22, 4, 76, 82]
 const soma = a + b + c + d
+
 console.log(soma);
+console.log(a + d);
+console.log(b*a);
+console.log(c-b);
+console.log(d/a);
+console.log(d%b);
