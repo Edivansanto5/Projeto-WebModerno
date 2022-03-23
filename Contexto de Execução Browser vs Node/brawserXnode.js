2df052c5407a8c89f8c82a1030e03a9d2d5fc0e6
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-23 10:32:07 -0300

    finalizando aula de node x js

---
+++ Contexto de Execução Browser vs Node/brawserXnode.js
@@ -0,0 +1,15 @@
+let a = 234
+global.b = 234
+
+this.c = 456
+this.d = false
+this.e = 'texte'
+
+console.log(a)
+console.log(global.b)
+console.log(this.c)
+console.log(module.exports.c);
+console.log(module.exports === this);
+console.log(module.exports == this);
+console.log(module.exports);
+
