9b78133f14edf89b9386881ae0a05dcc20fc4352
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-25 00:00:37 -0300

    finalizando aula de if e else simpless

---
+++ Estrutura de controle/ifElse02.js
@@ -0,0 +1,11 @@
+function somarNumeros(num1,nu2){
+    soma = num1 + nu2
+    media = soma/2
+    if (media >= 8){
+        console.log('Aprovado com sucesso parabens!!!!')
+    }else{
+        console.log('reprovado direto seu burro!!!! ');
+    }
+
+}
+somarNumeros(10,6)
