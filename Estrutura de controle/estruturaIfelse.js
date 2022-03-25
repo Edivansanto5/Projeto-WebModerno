1b7626afa0ac3df11dd25ce0d2dc16e4afc3ea2a
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-24 23:18:06 -0300

    finalizando  pasta de if e else

---
+++ Estrutura de controle/estruturaIfelse.js
@@ -0,0 +1,9 @@
+function mediaNota(nota1,nota2){
+    let media = (nota1 + nota2)/2
+    if(media>=7){
+        console.log('Aprovado com a média ' + media);
+    }else{
+        console.log('Você está reprovado ou está de prova final');
+    }
+
+}
