23f04628ac62f180f54f55b87f0832ad831cdb62
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-24 23:41:40 -0300

    finalizando pasta de cuidado com if

---
+++ Estrutura de controle/cuidadoComIfs.js
@@ -0,0 +1,18 @@
+function numeros(num){
+    if(num > 7)
+        console.log('Aprovado');
+        console.log(num);
+}
+//numeros(9)
+//numeros(5)
+
+function duvidas (num){
+    if (duvidas > 7);{
+        console.log('Aprovado');
+        console.log(num);
+    }
+    
+}
+// ; não tem que existir no bloco if
+duvidas(4)
+
